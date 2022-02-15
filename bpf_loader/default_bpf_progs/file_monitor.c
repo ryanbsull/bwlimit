e9e7877d938d7cd778596e55375cd473f9ca62bc
ryanbs183 <ryan.sullivan.183@gmail.com>
2022-02-15 15:45:33 -0500

    reworked timing calculation

--- bpf_loader/default_bpf_progs/file_monitor.c
+++ bpf_loader/default_bpf_progs/file_monitor.c
@@ -11,23 +11,34 @@
 BPF_PERF_OUTPUT(data);
 BPF_ARRAY(limit, u64, 1);
 BPF_ARRAY(time, u64, 1);
+BPF_ARRAY(tp, u64, 1);
 
 int wr_lim(size_t bytes){
-	u64 key = 0, *prev_ts, *lim, ts;
+	u64 key = 0, *prev_ts, *lim, *dt, ts;
 	
 	lim = limit.lookup(&key);
+    if(!lim)
+        return -1;
 	*lim += bytes;
 
 	prev_ts = time.lookup(&key);
+    if(!prev_ts)
+        return -1;
 
 	if(*lim > BWLIM_GLOBAL){
 		*lim = 0;
 		usleep(1000);
 	}
+    dt = tp.lookip(&key);
+    if(!dt)
+        return -1;
+
 	ts = bpf_ktime_get_ns();
-	*prev_ts = *prev_ts + (ts - *prev_ts);
-	if(*prev_ts > REFRESH_TIME_NS){
-		*prev_ts = 0;
+	*dt += ts - *prev_ts;
+    *prev_ts = ts;
+    
+	if(*dt > REFRESH_TIME_NS){
+		*dt = 0;
 		*lim = 0;
 	}
 	return 0;
